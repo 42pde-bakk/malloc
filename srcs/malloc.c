4c04fc1158ed8e2b0de4bfe2baf358acc3dd6a1e
pde-bakk <pde-bakk@student.codam.nl>
2022-02-14 21:09:13 +0100

    started with mmap

--- srcs/malloc.c
+++ srcs/malloc.c
@@ -6,7 +6,17 @@
 #include <stdio.h>
 
 void* malloc(size_t size) {
-    (void)size;
-    printf("size = %lu\n", size);
-    return (NULL);
+    int pagesize = getpagesize();
+
+    void* ptr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_ANON, -1, 0);
+    /* addr = NULL
+     * len = size
+     * prot = PROT_READ | PROT_WRITE
+     * flags = MAP_ANON
+     * fd = -1
+     * offset = 0
+    */
+    if (ptr == MAP_FAILED)
+        return (NULL);
+    return (ptr);
 }
