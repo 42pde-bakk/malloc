0e62b5ab732d6198136bca211c1a754a76a3464e
pde-bakk <pde-bakk@student.codam.nl>
2022-02-28 22:39:50 +0100

    Added structs for allocs and zones

--- include/peer_stdlib.h
+++ include/peer_stdlib.h
@@ -11,6 +11,28 @@
 # include <unistd.h> // getpagesize (2/3)
 # include <sys/resource.h> // getrlimit (2)
 
+# include <sys/errno.h> // TODO: REMOVE
+extern int errno;
+
+typedef struct s_alloc {
+  void*     ptr;
+  size_t    size;
+  int       freed;
+
+  struct s_alloc* prev;
+  struct s_alloc* next;
+} Alloc;
+
+typedef struct s_zone {
+    void*       start;
+    size_t      size;
+    size_t      allocated_size;
+    size_t      allocations_amount;
+
+    struct s_zone* prev;
+    struct s_zone* next;
+} Zone;
+
 void free(void *ptr);
 void *malloc(size_t size);
 void *realloc(void *ptr, size_t size);
