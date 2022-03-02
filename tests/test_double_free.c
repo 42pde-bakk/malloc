856800bebb001bc5e9d9660ff4b7bdb48c922711
pde-bakk <pde-bakk@student.codam.nl>
2022-03-03 00:29:18 +0100

    moved tests to tests folder

---
+++ tests/test_double_free.c
@@ -0,0 +1,16 @@
+//
+// Created by Peer De bakker on 3/2/22.
+//
+
+#include "peer_stdlib.h"
+#include <stdio.h>
+
+int	main() {
+	void	*ptr = malloc(88);
+	printf("ptr = %p\n", ptr);
+	free(ptr);
+	printf("after first free\n");
+	free(ptr);
+	printf("after second free\n");
+	return (0);
+}
