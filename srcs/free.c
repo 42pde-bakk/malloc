8678a779a2ea0108bf38431565735a4814cdab08
pde-bakk <pde-bakk@student.codam.nl>
2022-03-04 00:01:14 +0100

    feat: if we free the last block in a t_zone, we now munmap the zone IF there is another active zone in the linked list (this way we always have some preallocated memory)

--- srcs/free.c
+++ srcs/free.c
@@ -4,11 +4,26 @@
 
 #include "peer_stdlib.h"
 #include <stdio.h>
+#include <assert.h>
 
 static void	error_free(void *ptr) {
 	dprintf(2, "malloc *** error for object %p: pointer being freed was not allocated\n", ptr);
 	dprintf(2, "malloc: *** set a breakpoint in malloc_error_break to debug\n");
 	// cleanup
+}
+
+void	release_zone(t_zone *zone) {
+	if (zone->prev)
+		zone->prev->next = zone->next;
+	if (zone->next)
+		zone->next->prev = zone->prev;
+	int ret = munmap((void *)zone, zone->total_size);
+	if (ret) {
+		// TODO: remove
+		perror("munmap");
+		printf("tried to call munmap(%p, %zu)\n", (void*)zone, zone->total_size);
+		assert(0);
+	}
 }
 
 void    free(void* ptr) {
@@ -26,5 +41,13 @@ void    free(void* ptr) {
 		return (error_free(ptr));
 
 	release_block(block, zone);
+
+	if (zone->block_count == 0 && (zone->prev || zone->next)) {
+		// munmap the zone if it contains no blocks
+		// but only if it isn't the only zone of it's size
+		// Will this also work for my large ones?
+		release_zone(zone);
+	}
 	// try to combine freed blocks if possible (or is that bonus)
+
 }
