4f2087c63135914afb1d350e9f39a349ed6bde2c
pde-bakk <pde-bakk@student.codam.nl>
2022-03-04 00:54:14 +0100

    changed malloc because find_spot() now returns the void* ptr

--- srcs/malloc.c
+++ srcs/malloc.c
@@ -5,12 +5,6 @@
 #include "peer_stdlib.h"
 #include <stdio.h>
 
-
 void* malloc(size_t size) {
-    t_block	*block = find_spot(size);
-    if (!block)
-		return (NULL);
-    void	*ptr = BLOCK_SHIFT((void *)block);
-//    printf("malloc: block=%p, ptr = %p\n", (void*)block, ptr);
-    return (ptr);
+	return (find_spot(size));
 }
